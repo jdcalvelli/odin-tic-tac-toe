b192ee4b18c51da8607b315c3ba0f4cc6b460ccb
JD Calvelli <jdcalvelli@gmail.com>
2022-02-23 13:27:01 -0500

    add win evaluation function in stateManager

--- script.js
+++ script.js
@@ -30,13 +30,50 @@ const StateManager = (() => {
         while (winningPlayer == null) {
             if (currentTurn == 'player1') {
                 player1.playTurn();
+                evaluateWin(player1);
                 currentTurn = 'player2';
             } else if (currentTurn == 'player2') {
                 player2.playTurn();
+                evaluateWin(player2);
                 currentTurn = 'player1';
             }
         }
-    }
+    };
+
+    const evaluateWin = (player) => {
+        if (player.value == GameBoard.board[0] && player.value == GameBoard.board[1] && player.value == GameBoard.board[2]) {
+            winningPlayer = player.value;
+            alert(`congrats ${winningPlayer}, you win`);
+        }
+        else if (player.value == GameBoard.board[3] && player.value == GameBoard.board[4] && player.value == GameBoard.board[5]) {
+            winningPlayer = player.value;
+            alert(`congrats ${winningPlayer}, you win`);
+        }
+        else if (player.value == GameBoard.board[6] && player.value == GameBoard.board[7] && player.value == GameBoard.board[8]) {
+            winningPlayer = player.value;
+            alert(`congrats ${winningPlayer}, you win`);
+        }
+        else if (player.value == GameBoard.board[0] && player.value == GameBoard.board[3] && player.value == GameBoard.board[6]) {
+            winningPlayer = player.value;
+            alert(`congrats ${winningPlayer}, you win`);
+        }
+        else if (player.value == GameBoard.board[1] && player.value == GameBoard.board[4] && player.value == GameBoard.board[7]) {
+            winningPlayer = player.value;
+            alert(`congrats ${winningPlayer}, you win`);
+        }
+        else if (player.value == GameBoard.board[2] && player.value == GameBoard.board[5] && player.value == GameBoard.board[8]) {
+            winningPlayer = player.value;
+            alert(`congrats ${winningPlayer}, you win`);
+        }
+        else if (player.value == GameBoard.board[0] && player.value == GameBoard.board[4] && player.value == GameBoard.board[8]) {
+            winningPlayer = player.value;
+            alert(`congrats ${winningPlayer}, you win`);
+        }
+        else if (player.value == GameBoard.board[2] && player.value == GameBoard.board[4] && player.value == GameBoard.board[6]) {
+            winningPlayer = player.value;
+            alert(`congrats ${winningPlayer}, you win`);
+        }
+    };
 
     return {
         turnManager
